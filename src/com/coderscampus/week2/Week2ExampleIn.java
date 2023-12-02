c784a5f43ed1950926aa2f8895f62d849475d3fd
svander7 <147276430+svander7@users.noreply.github.com>
2023-12-01 23:05:10 -0500

    Update Week2ExampleIn.java
    Finished Unit 2-12

--- src/com/coderscampus/week2/Week2ExampleIn.java
+++ src/com/coderscampus/week2/Week2ExampleIn.java
@@ -1,8 +1,36 @@
 package com.coderscampus.week2;
 
+import java.util.Scanner;
+
 public class Week2ExampleIn {
 		
 	public static void main  (String[] args)  {
 		
+		// This is just for teaching purposes /////////
+		// With respect to imports / packages   ///////
+		// java.lang is by default imported for us ////
+		//java.lang.String aString = "Hello World!"; //
+		//String anotherString = "Another String";   //
+		///////////////////////////////////////////////
+		
+		//Scanner scanner = new Scanner(System.in);
+		//System.out.println("Type in any input:");
+		//String userInput = scanner.nextLine();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type a number between 50 and 300");
+		String userInput = scanner.nextLine();
+		Integer convertedInput = Integer.parseInt(userInput);
+		
+			
+		
+			if (convertedInput >= 50 && convertedInput <= 300)	{
+				System.out.println("Yes!");
+			} else if (convertedInput < 50) {
+				System.out.println("No!");
+			} else if (convertedInput > 300) { 
+				System.out.println("No!");
+			} 
+			
 	}
 }
