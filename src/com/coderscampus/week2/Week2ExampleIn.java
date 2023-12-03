7ab2e2f6816b3baeba1c39c3d7eaac41fad52872
svander7 <147276430+svander7@users.noreply.github.com>
2023-12-03 17:01:34 -0500

    Final

--- src/com/coderscampus/week2/Week2ExampleIn.java
+++ src/com/coderscampus/week2/Week2ExampleIn.java
@@ -13,24 +13,33 @@ public class Week2ExampleIn {
 		//String anotherString = "Another String";   //
 		///////////////////////////////////////////////
 		
-		//Scanner scanner = new Scanner(System.in);
-		//System.out.println("Type in any input:");
-		//String userInput = scanner.nextLine();
-		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Type a number between 50 and 300");
-		String userInput = scanner.nextLine();
-		Integer convertedInput = Integer.parseInt(userInput);
+		System.out.println("Type in Your Name:");
+		String name = scanner.nextLine();
+		System.out.println("Type in Your Age:");
+		String age = scanner.nextLine();
 		
-			
+		//Scanner scanner = new Scanner(System.in);
+		//System.out.println("Type a number between 50 and 300");
+		//String userInput = scanner.nextLine();
+		//Integer convertedInput = Integer.parseInt(userInput);
 		
-			if (convertedInput >= 50 && convertedInput <= 300)	{
-				System.out.println("Yes!");
-			} else if (convertedInput < 50) {
-				System.out.println("No!");
-			} else if (convertedInput > 300) { 
-				System.out.println("No!");
-			} 
+			//if (convertedInput >= 50 && convertedInput <= 300)	{
+			//	System.out.println("Yes!");
+			//} else if (convertedInput < 50) {
+			//	System.out.println("No!");
+			//} else if (convertedInput > 300) { 
+			//	System.out.println("No!");
+			//} 
 			
+		if (name.equals("Trevor") 
+				&& age.equals("36")
+				|| name.equals("TJ")) {
+			System.out.println("Holy cow it's me!");
+		} else {
+			System.out.println("Hello: " + name);
+		}
+		
+		scanner.close();
 	}
 }
